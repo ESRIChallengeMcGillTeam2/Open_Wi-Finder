8bd6cc232e750bb234b5b31744f067ccbc823d98
Robert Smith <robert.smith8@mail.mcgill.ca>
2015-04-23 06:15:01 -0400

    fixedDataExport
    added export.js

--- js/map.js
+++ js/map.js
@@ -5,9 +5,20 @@
 * [CodyHouse Full-Screen Pop-Out Navigation](http://codyhouse.co/gem/full-screen-pop-out-navigation/)
 * [Firebase ](https://www.firebase.com) 
 
+#### Written by:
+* Robert Smith
+* Suthee Sangiambut
+* Matt Kalebic
+
+#### Produced for Esri's ECCE App Challenge 2015, Submitted March 6th 2015
+
+#### Last Updated: March 19th 2015
+
 -------------------------------- */
 
 // JavaScript Document
+
+//global variable initialization
 var map;
 var graphic;
 var currLocation;
@@ -17,10 +28,12 @@ var myFirebase = new Firebase("https://open-wifinder.firebaseio.com/");
 var lon;
 var lat;
 
+//default lat/lon in case users have geolocation disabled
 lon = "-73.5673";
 lat = "45.5017";
 
 require([
+	  //modules to load -- aliases defined after "function ("
       "esri/map", 
       "esri/dijit/LocateButton",
       "esri/geometry/Point", 
@@ -43,22 +56,27 @@ require([
 	  "dojo/on", 
 	  "dojo/_base/lang", "dojo/domReady!" 
     ], function(
-      Map, LocateButton, Point,
-        SimpleMarkerSymbol, SimpleLineSymbol,
-        Graphic, Color, //Geometry Hooks
-    Geometry,
-    Polyline, 
-    Polygon,
+      //module aliases to be used when calling them inside the require function
+	  Map, 
+	  LocateButton, 
+	  Point,
+      SimpleMarkerSymbol, 
+	  SimpleLineSymbol,
+      Graphic, 
+	  Color, //Geometry Hooks
+      Geometry,
+      Polyline, 
+      Polygon,
  
     //Graphic & Style Hooks
-    SimpleFillSymbol,
-    InfoTemplate, 
-	dom, 
-	on, 
-	lang,
-	domReady
+	  SimpleFillSymbol,
+	  InfoTemplate, 
+	  dom, 
+	  on, 
+	  lang,
+	  domReady
     )  {
-
+		//new map object with settings. map placed in div with id="map"
       map = new Map("map", {
         center: [lon, lat],
         zoom: 12,
@@ -66,13 +84,16 @@ require([
       });
       
 	  map.on("load", initFunc);
-	  loadAllPoints();
-	        
+	  loadAllPoints();//draws all points to the map
+	   
+	   
+	   //Creates geolocate button using ersi Geolocation dijit     
       geoLocate = new LocateButton({
         map: map
       }, "LocateButton");
       geoLocate.startup();
 	  
+	  //html-JS interaction event handlers
 	   var addPointBtn = dom.byId("addPointPopup");
 	  on(addPointBtn, "click", div_show);
 	  
@@ -86,7 +107,7 @@ require([
 	  on(closeDiv, "click", div_hide);
 	  
 	  
-
+		//Hotspot object definition and constructor
 	  var Hotspot = function (lon, lat, ssid, auth, avail, openhr, closedhr) {
 
   		this.lon = lon;
@@ -104,57 +125,58 @@ require([
 	  var graphic;
 
 
-
+	//function to pull data from the form into a Hotspot object, push that object to firebase and center the map at the newly created point. Currently only takes location data from the current user position.
 	function submitData() {
-	if (dom.byId('ssid').value == "" || dom.byId('auth').value == "" || dom.byId('avail').value == "") {
-		alert("Fill All Fields !");
-	} 
-	else {
-	
-	
-	var createHotspot = new Hotspot(currLoc.x,currLoc.y, dom.byId("ssid").value, dom.byId("auth").value, dom.byId("avail").value, dom.byId("openhr").value, dom.byId("closedhr").value);
-	console.log(createHotspot);
-	pushToFirebase(createHotspot);
-	map.centerAt(createHotspot.pt);
-	//dom.byId('form').submit();
-	alert("New Hotspot Submitted Successfully!");
-	document.getElementById("ssid").reset();
-	document.getElementById("auth").reset();
-	document.getElementById("avail").reset();
-	document.getElementById("openhr").reset();
-	document.getElementById("closedhr").reset();
-	document.getElementById("form").reset();
-	
+	  if (dom.byId('ssid').value == "" || dom.byId('auth').value == "" || dom.byId('avail').value == "") {
+		  alert("Fill All Fields !");
+	  } 
+	  else {
+		var createHotspot = new Hotspot(currLoc.x,currLoc.y, dom.byId("ssid").value, dom.byId("auth").value, dom.byId("avail").value, dom.byId("openhr").value, dom.byId("closedhr").value);
+		console.log(createHotspot);
+		pushToFirebase(createHotspot);
+		map.centerAt(createHotspot.pt);
+		//dom.byId('form').submit();
+		
+		document.getElementById("ssid").reset();
+		document.getElementById("auth").reset();
+		document.getElementById("avail").reset();
+		document.getElementById("openhr").reset();
+		document.getElementById("closedhr").reset();
+		document.getElementById("form").reset();
+		
+		alert("New Hotspot Submitted Successfully!");
+		div_hide();
+	  }
 	}
-	div_hide();
-	}
+	
 	//Function To Display Popup
 	function div_show() {
-	document.getElementById("popupDiv").style.display = "block";
+		document.getElementById("popupDiv").style.display = "block";
 	}
 	//Function to Hide Popup
 	function div_hide(){
-	dom.byId("popupDiv").style.display = "none";
-	document.getElementById("ssid").reset();
-	document.getElementById("auth").reset();
-	document.getElementById("avail").reset();
-	document.getElementById("openhr").reset();
-	document.getElementById("closedhr").reset();
-	document.getElementById("form").reset();
+		dom.byId("popupDiv").style.display = "none";
+		
+		//These resets don't seem to do anything. Need to correct form reset.
+		document.getElementById("ssid").reset();
+		document.getElementById("auth").reset();
+		document.getElementById("avail").reset();
+		document.getElementById("openhr").reset();
+		document.getElementById("closedhr").reset();
+		document.getElementById("form").reset();
 	}
 
 	//draw() function draws all hotspots in the hotspotList[]
-	function draw () {
-		
+	function draw () {		
 		if (map.graphics){map.graphics.clear();}
 		for (i=0; i<hotspotList.length; i++){
 			var attr = {"SSID":hotspotList[i].ssid,"Auth":hotspotList[i].auth,"Avail":hotspotList[i].avail,"Open":hotspotList[i].openhr,"Closed":hotspotList[i].closedhr};
-	var hotspotInfoBox = new InfoTemplate("Hotspot Details","<strong>SSID: </strong> ${SSID}  <br/> <strong>Type:</strong> ${Auth} <br/> <strong>Availability:</strong> ${Avail} <br/><strong>Open:</strong>${Open}</br><strong>Closed:</strong>${Closed}");
-			addGraphic(hotspotList[i].pt, attr, hotspotInfoBox);
+		var hotspotInfoBox = new InfoTemplate("Hotspot Details","<strong>SSID: </strong> ${SSID}  <br/> <strong>Type:</strong> ${Auth} <br/> <strong>Availability:</strong> ${Avail} <br/><strong>Open:</strong>${Open}</br><strong>Closed:</strong>${Closed}");
+		addGraphic(hotspotList[i].pt, attr, hotspotInfoBox);
 		}
-		
 	}
 	
+	//finds nearest neighbor using euclidian distance, then center on that point. Needs to be improved to be more appropriate for working with lat/lon
 	function closestDist (){
 		var minEuclidDist;
 		var closestIndex=0;
@@ -177,30 +199,31 @@ require([
 				closestIndex = i;
 			};	
 		} //end for loop
-		map.centerAndZoom(hotspotList[closestIndex].pt,15);
+		map.centerAndZoom(hotspotList[closestIndex].pt,10);
 		
 		//addGraphic(hotspotList[closestIndex].pt);
 		
 	}
 	function orientationChanged() {
-          if(map){
-            map.reposition();
-            map.resize();
-          }
-        }
+		if(map){
+		  map.reposition();
+		  map.resize();
+		}
+	}
 		
 		//geolocation to run at map load
 
-        function initFunc(map) {
-          if( navigator.geolocation ) {  
-            navigator.geolocation.getCurrentPosition(zoomToLocation, locationError);
-			
-			//watches for movement and updates current location
-            watchId = navigator.geolocation.watchPosition(watchLocation, locationError); //Turns off watchPosition when commented
-          } else {
-            alert("Browser doesn't support Geolocation. Visit http://caniuse.com to see browser support for the Geolocation API.");
-          }
-        }
+	function initFunc(map) {
+		if( navigator.geolocation ) {  
+		  navigator.geolocation.getCurrentPosition(zoomToLocation, locationError);
+		  
+		  //watches for movement and updates current location
+		  watchId = navigator.geolocation.watchPosition(watchLocation, locationError); //Turns off watchPosition when commented
+		} 
+		else {
+		  alert("Browser doesn't support Geolocation. Visit http://caniuse.com to see browser support for the Geolocation API.");
+		}
+	}
 //error handling for geolocation
         function locationError(error) {
           //error occurred so stop watchPosition
@@ -274,21 +297,13 @@ require([
           map.centerAt(pt);
         }
 		
-	function addPoint(lon, lat, ssid, auth, avail){
-	/* if(map){alert("Map OK" + "\nAdding Point" + "\nAdding Info Box" + "\n\nClick point to show info window");}
-	*/var hotspotPoint = new Point(lon, lat);
-	var attr = {"SSID":ssid,"Authorization":auth,"Availability":avail};
-	var hotspotInfoBox = new InfoTemplate("Hotspot Details","<strong>Network Name: </strong> ${SSID}  <br/> <strong>Auth Level:</strong> ${Authorization} <br/> <strong>Availability:</strong> ${Availability}");
-	/* map.centerAt(hotspotPoint); */
-	addGraphic(hotspotPoint, attr, hotspotInfoBox); 
-	}
-
+	
+//this function grabs a 'snapshot' of all the data in Firebase, then navigates down to the 'features' child. It then iterates through all the children under 'attributes' and retrieves all attribute data. Then it converts them to strings or numbers and calls addPoint to map them
 	function loadAllPoints(){
 
 		var dataObject;
 		hotspotList=[];
-	//this function grabs a 'snapshot' of all the data in Firebase, then navigates down to the 'features' child. It then iterates through all the
-	//children under 'attributes' and retrieves all attribute data. Then it converts them to strings or numbers and calls addPoint to map them
+	
 		myFirebase.on("value", function(snapshot) {
 		dataObject = snapshot;
 		
@@ -325,13 +340,14 @@ require([
 	});//end firebase.on()
 	}//end loadAllPoints
 	
+	//Function to push data from hotspot object to firebase
 	function pushToFirebase(hotspot){
 		myFirebase.push({"SSID":hotspot.ssid,"Latitude":hotspot.lat,"Longitude":hotspot.lon,"Authentication":hotspot.auth,"Availability":hotspot.avail,"openhr":hotspot.openhr,"closedhr":hotspot.closedhr});
 		loadAllPoints();
 	}
         
         
-      });//end BIG FUNCTION
+      });//end BIG FUNCTION (dojo require)
 
 	  
 
